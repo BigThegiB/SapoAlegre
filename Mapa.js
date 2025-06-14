0a73e44df0c900a26a1900e6aa65682ac740ee16
Gib <brunorogerio.moretoni@gmail.com>
2025-06-14 18:08:58 -0300

    Backup pq eu vo fazer bosta

--- Mapa.js
+++ Mapa.js
@@ -3,27 +3,27 @@ google.charts.setOnLoadCallback(DesenharMapa);
 const TelaZoom = document.getElementById("TelaZoom");
 let qntZoom = 1;
 let Zoom = false;
-let mouseX = 0;
 let mouseY = 0;
 
 document.addEventListener("mousemove", (TrackMovimento) => {
   mouseY = TrackMovimento.clientY;
 });
+
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 function DesenharMapa() {
   var data = google.visualization.arrayToDataTable([
-    ["Continente", "Nome"],
-    ["142", "Ásia"],
-    ["150", "Europa"],
-    ["002", "África"],
-    ["019", "América"],
-    ["009", "Oceania"],
-    ["005", "América do Sul"],
-    ["013", "América Central"],
-    ["021", "América do Norte"],
+    ["Continente", "Sapos no Mundo"],
+    ["142", "Sapos na Ásia"],
+    ["150", "Sapos na Europa"],
+    ["002", "Sapos na África"],
+    ["019", "Sapos na América"],
+    ["009", "Sapos na Oceania"],
+    ["005", "Sapos na América do Sul"],
+    ["013", "Sapos na América Central"],
+    ["021", "Sapos na América do Norte"],
   ]);
 
   var options = {
@@ -55,7 +55,7 @@ function DesenharMapa() {
       TelaZoom.classList.remove("active");
       options.resolution = resolucao;
       options.region = continenteID;
-      MapDraw.draw(data, options);
+      MapDraw.draw(data, options); 
       console.log(options.region);
     });
   }
@@ -63,7 +63,9 @@ function DesenharMapa() {
   function ClickUser() { // Lida com o click do usuario em cada continente
     var Selecionado = MapDraw.getSelection();
     var ValorSelecionado = data.getValue(Selecionado[0].row, 0);
+    if (!Zoom) {
     switch (ValorSelecionado) {
+      
       case "142": // Ásia
         ZoomMapa(0.6, 0.53, 3, "continents", "142");
         break;
@@ -86,7 +88,8 @@ function DesenharMapa() {
       case "009":// Oceania
         ZoomMapa(0.7, 0.68, 3, "continents", "009");
         break;
-    }
+    }}
+    if (Zoom){options.tooltip = { trigger: 'none' };}
   }
 }
 
